a9aaef367f836f76786e042b4bdfab6b73d8fb4b
류창훈 <107829554+Ryuchanghoon@users.noreply.github.com>
2024-01-01 04:06:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-import cv2
 import mediapipe as mp
 import numpy as np
+import imageio
 import tempfile
 
 # 각도 계산 함수
@@ -12,7 +12,6 @@ def calculate_angle(landmark1, landmark2):
     angle_degrees = np.degrees(angle_radians)
     return angle_degrees
 
-
 # 좌표 추출 함수
 def extract_coordinates(results):
     landmarks = results.pose_landmarks.landmark
@@ -22,43 +21,35 @@ def extract_coordinates(results):
     }
     return coordinates
 
-# 비디오 처리 함수
+# imageio를 사용한 비디오 처리 함수
 def process_video(video_path):
-    mp_pose = mp.solutions.pose
-    pose = mp_pose.Pose()
-
-    cap = cv2.VideoCapture(video_path)
-    if not cap.isOpened():
-        return ["Error: Cannot open video."]
-
+    mp_pose = mp.solutions.pose.Pose()
+    
+    reader = imageio.get_reader(video_path)
     extracted_coordinates = []
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = pose.process(image_rgb)
-
-        if results.pose_landmarks:
-            coordinates = extract_coordinates(results)
-            extracted_coordinates.append(coordinates)
-
-    cap.release()
+    try:
+        for frame in reader:
+            image_rgb = frame
+            results = mp_pose.process(image_rgb)
+            if results.pose_landmarks:
+                coordinates = extract_coordinates(results)
+                extracted_coordinates.append(coordinates)
+    except RuntimeError: # 종료 프레임에 도달할 경우
+        pass
+    reader.close()
     return extracted_coordinates
 
 # streamlit 실행 함수
 def main():
-    st.title("Extract coordinate")
-
+    st.title("Pose Estimation with MediaPipe")
+    
     uploaded_file = st.file_uploader("Upload a video file", type=["mp4", "avi"])
-
+    
     if uploaded_file is not None:
         with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as tmpfile:
             tmpfile.write(uploaded_file.read())
             video_path = tmpfile.name
 
-       
         st.video(video_path)
         results = process_video(video_path)
         for coordinates in results:
